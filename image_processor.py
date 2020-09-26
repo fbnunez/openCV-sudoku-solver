c35d8d84d6aa326b9d3685cf4278dace82aed46c
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-09-26 17:21:30 -0300

    Finding vertical lines and removing duplicates

--- image_processor.py
+++ image_processor.py
@@ -1,27 +1,47 @@
 import imutils
 import cv2
 import numpy
+DEFAULT_HEIGHT, DEFAULT_WIDTH = 500, 500
 
 
 def main():
     image = cv2.imread("./img/sudoku.png")
-    image = imutils.resize(image, height=500)
-    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edgedImage = cv2.Canny(grayImage, threshold1=100,
-                           threshold2=200, apertureSize=3)
+    resizedImage = imutils.resize(
+        image, height=DEFAULT_HEIGHT, width=DEFAULT_WIDTH)
+    grayImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
+    edgedImage = cv2.Canny(grayImage, threshold1=50,
+                           threshold2=200)
+    verticalLines = getVerticalLines(edgedImage, resizedImage)
 
+
+def getVerticalLines(edgedImage, resizedImage):
     # VERTICAL LINES
     # 16 lines + doubles
     lines = cv2.HoughLinesP(image=edgedImage, rho=1, theta=numpy.pi,
-                            threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=5)
+                            threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=250)
+    linesToKeep = []
+    PIXEL_TRESHOLD = 10
+    for line1 in lines:
+        for line2 in lines:
+            if line1[0][0] < line2[0][0] and line1[0][0] + PIXEL_TRESHOLD > line2[0][0]:
+                linesToKeep.append(line1)
+            elif line1[0][0] < PIXEL_TRESHOLD:
+                linesToKeep.append(line1)
+            elif line1[0][0] >= DEFAULT_WIDTH - PIXEL_TRESHOLD:
+                linesToKeep.append(line1)
+    linesToKeep.append(lines[len(lines)-1])
+    linesToKeep = numpy.array(linesToKeep)
+    print(lines)
+    negativeImage(linesToKeep, resizedImage)
+    return linesToKeep
 
 
 def negativeImage(lines, image):
     negativeImage = numpy.zeros_like(image)
     for line in lines:
         coords = line[0]
-        x1, y1, x2, y2 = coords[0], coords[1], coords[2], coords[3]
-        cv2.line(negativeImage, (x1, y1), (x2, y2), (0, 255, 0), 1)
+        x1, y1, x2, y2 = line[0]
+        cv2.line(negativeImage, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.imshow("Input", negativeImage)
     cv2.waitKey(0)
 
