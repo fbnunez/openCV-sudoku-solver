cc40e565e01f081ef238553d62af9b4c597bd961
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-09-26 18:35:10 -0300

    Finding horizontal lines and removing duplicates

--- image_processor.py
+++ image_processor.py
@@ -12,6 +12,30 @@ def main():
     edgedImage = cv2.Canny(grayImage, threshold1=50,
                            threshold2=200)
     verticalLines = getVerticalLines(edgedImage, resizedImage)
+    horizontalLines = getHorizontal(edgedImage, resizedImage)
+
+
+def getHorizontal(edgedImage, resizedImage):
+    lines = cv2.HoughLinesP(image=edgedImage, rho=1, theta=numpy.pi/2,
+                            threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=250)
+    horizontalLines = []
+    PIXEL_TRESHOLD = 10
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        if abs(y1 - y2) == 0:
+            horizontalLines.append(line)
+    horizontalLines = numpy.array(horizontalLines)
+    linesToKeep = []
+    for line1 in horizontalLines:
+        for line2 in horizontalLines:
+            if line1[0][1] < line2[0][1] and line1[0][1] + PIXEL_TRESHOLD > line2[0][1]:
+                linesToKeep.append(line1)
+            elif line1[0][1] < PIXEL_TRESHOLD:
+                linesToKeep.append(line1)
+            elif line1[0][1] >= DEFAULT_HEIGHT - PIXEL_TRESHOLD:
+                linesToKeep.append(line1)
+    # negativeImage(linesToKeep, resizedImage)
+    return linesToKeep
 
 
 def getVerticalLines(edgedImage, resizedImage):
@@ -31,8 +55,8 @@ def getVerticalLines(edgedImage, resizedImage):
                 linesToKeep.append(line1)
     linesToKeep.append(lines[len(lines)-1])
     linesToKeep = numpy.array(linesToKeep)
-    print(lines)
-    negativeImage(linesToKeep, resizedImage)
+    # print(lines)
+    # negativeImage(linesToKeep, resizedImage)
     return linesToKeep
 
 
