f1e975359233323e475006b286df147fe6731521
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-09-26 16:26:05 -0300

    Detecting and showing vertical lines

--- image_processor.py
+++ image_processor.py
@@ -1,9 +1,21 @@
 import imutils
 import cv2
+import numpy
 
 image = cv2.imread("./img/sudoku.png")
 image = imutils.resize(image, height=500)
 grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Input", grayImage)
+edgedImage = cv2.Canny(grayImage, threshold1=100,
+                       threshold2=200, apertureSize=3)
 
+# 16 lines
+lines = cv2.HoughLinesP(image=edgedImage, rho=1, theta=numpy.pi,
+                        threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=5)
+
+negativeImage = numpy.zeros_like(image)
+for line in lines:
+    coords = line[0]
+    cv2.line(negativeImage, (coords[0], coords[1]),
+             (coords[2], coords[3]), (0, 255, 0), 1)
+cv2.imshow("Input", negativeImage)
 cv2.waitKey(0)
