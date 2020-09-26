c891bae102b31a0cff8009fbb5ec5969f1a4450f
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-09-26 16:49:23 -0300

    negativeImage function to test line detection

--- image_processor.py
+++ image_processor.py
@@ -8,14 +8,19 @@ grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 edgedImage = cv2.Canny(grayImage, threshold1=100,
                        threshold2=200, apertureSize=3)
 
+# VERTICAL LINES
 # 16 lines
 lines = cv2.HoughLinesP(image=edgedImage, rho=1, theta=numpy.pi,
                         threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=5)
 
-negativeImage = numpy.zeros_like(image)
-for line in lines:
-    coords = line[0]
-    cv2.line(negativeImage, (coords[0], coords[1]),
-             (coords[2], coords[3]), (0, 255, 0), 1)
-cv2.imshow("Input", negativeImage)
-cv2.waitKey(0)
+# TEST FUNCTION
+
+
+def negativeImage(lines, image):
+    negativeImage = numpy.zeros_like(image)
+    for line in lines:
+        coords = line[0]
+        x1, y1, x2, y2 = coords[0], coords[1], coords[2], coords[3]
+        cv2.line(negativeImage, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    cv2.imshow("Input", negativeImage)
+    cv2.waitKey(0)
