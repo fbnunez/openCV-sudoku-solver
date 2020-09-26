8aaaf3ff2a975dd401fe4bda0f3d5cbbffa94e7c
fbNunez <43887329+fbnunez@users.noreply.github.com>
2020-09-26 16:50:46 -0300

    refactoring base code into main() function

--- image_processor.py
+++ image_processor.py
@@ -2,18 +2,18 @@ import imutils
 import cv2
 import numpy
 
-image = cv2.imread("./img/sudoku.png")
-image = imutils.resize(image, height=500)
-grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edgedImage = cv2.Canny(grayImage, threshold1=100,
-                       threshold2=200, apertureSize=3)
 
-# VERTICAL LINES
-# 16 lines
-lines = cv2.HoughLinesP(image=edgedImage, rho=1, theta=numpy.pi,
-                        threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=5)
+def main():
+    image = cv2.imread("./img/sudoku.png")
+    image = imutils.resize(image, height=500)
+    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edgedImage = cv2.Canny(grayImage, threshold1=100,
+                           threshold2=200, apertureSize=3)
 
-# TEST FUNCTION
+    # VERTICAL LINES
+    # 16 lines + doubles
+    lines = cv2.HoughLinesP(image=edgedImage, rho=1, theta=numpy.pi,
+                            threshold=100, lines=numpy.array([]), minLineLength=400, maxLineGap=5)
 
 
 def negativeImage(lines, image):
@@ -24,3 +24,6 @@ def negativeImage(lines, image):
         cv2.line(negativeImage, (x1, y1), (x2, y2), (0, 255, 0), 1)
     cv2.imshow("Input", negativeImage)
     cv2.waitKey(0)
+
+
+main()
